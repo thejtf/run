11ff9b5fb2665d9e1201d53ea4fa7e74b942365a
Jopus <chizongjtf@gmail.com>
2023-05-13 14:43:31 +0800

    change git
    push

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/jtf/run/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---src-pages-404-jsx": preferDefault(require("/Users/jtf/run/src/pages/404.jsx")),
+  "component---src-pages-index-jsx": preferDefault(require("/Users/jtf/run/src/pages/index.jsx"))
+}
+
